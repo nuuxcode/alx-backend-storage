9b5620132703a2d686b25ca2091cf379d7f66d9a
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-18 22:20:31 +0100

    😏🤩🤔 Task2 Best band ever!

--- 0-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- creates a 'users' table if it doesn't already exist.
-
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
