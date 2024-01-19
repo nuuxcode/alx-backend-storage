a32abef00a537c76ccfd17f1efdc37d4ba6ca7e0
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-19 22:25:33 +0100

    ☺☺☺ Task4 Buy buy buy

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- creates a trigger that decreases the quantity
+CREATE TRIGGER dec_q AFTER INSERT ON orders FOR EACH ROW
+BEGIN
+   UPDATE items
+   SET quantity = quantity - NEW.number
+   WHERE name = NEW.item_name;
+END;
