14a3c2e18702e0981590c48b49b6c96cd22b6718
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-20 00:52:03 +0100

    🙃😕😔 Task7 try difrenet

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -2,10 +2,8 @@
 DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE avg_score FLOAT;
-
-    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = user_id;
-
-    UPDATE users SET average_score = avg_score WHERE id = user_id;
+    UPDATE users
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id)
+    WHERE id = user_id;
 END;//
 DELIMITER ;
