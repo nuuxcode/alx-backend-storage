226b2670d4b42bcf33e5eabe1f9072d133090882
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-25 02:48:25 +0100

    🙄😶😑 Task0 no idea

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-"""doc doc"""
+"""doc doc module"""
+
 import redis
 import uuid
 from typing import Union
 
 
 class Cache:
-    """doc doc"""
+    """doc doc class"""
 
     def __init__(self):
-        """doc doc"""
+        """doc doc method"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: Union[str, float, bytes, int]) -> str:
-        """doc doc"""
+        """doc doc method"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
