c4ad2acc7b925c14063c98c2cccd20ebb04a2afe
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-25 02:40:09 +0100

    😪😪😪 switch file

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""doc doc"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """doc doc"""
+
+    def __init__(self):
+        """doc doc"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, float, bytes, int]) -> str:
+        """doc doc"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
-"""doc doc"""
+"""
+Main file
+"""
 import redis
-import uuid
-from typing import Union
 
+Cache = __import__('exercise').Cache
 
-class Cache:
-    """doc doc"""
+cache = Cache()
 
-    def __init__(self):
-        """doc doc"""
-        self._redis = redis.Redis()
-        self._redis.flushdb()
+data = b"hello"
+key = cache.store(data)
+print(key)
 
-    def store(self, data: Union[str, float, bytes, int]) -> str:
-        key = str(uuid.uuid4())
-        self._redis.set(key, data)
-        return key
+local_redis = redis.Redis()
+print(local_redis.get(key))
