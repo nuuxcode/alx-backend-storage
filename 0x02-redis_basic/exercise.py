07c8b5da4fc334ce0dcd0672fa0974def8aa2917
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2024-01-26 10:48:33 +0100

    😮😮😮 Add Documentation and order Unions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -63,7 +63,7 @@ class Cache:
 
     @count_calls
     @call_history
-    def store(self, data: Union[str, float, bytes, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """doc doc method"""
         keyx = str(uuid.uuid4())
         self._redis.set(keyx, data)
@@ -72,13 +72,16 @@ class Cache:
     def get(
         self, key: str, fn: Optional[Callable] = None
     ) -> Union[str, bytes, int, float]:
+        """doc doc method"""
         value = self._redis.get(key)
         if fn:
             value = fn(value)
         return value
 
     def get_str(self, key: str) -> str:
+        """doc doc method"""
         return self.get(key, fn=str)
 
     def get_int(self, key: str) -> int:
+        """doc doc method"""
         return self.get(key, fn=int)
